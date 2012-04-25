241c0ec081d8126cb15199f85205ee2c47e9dd9e
Elliot Saba <staticfloat@gmail.com>
2012-04-24 20:52:17 -0700

    Update to LAPACK 3.4.1 Actually install the library, so Cellar is now non-empty. :) Default to compilation against Accelerate, with option for OpenBLAS

--- lapack.rb
+++ lapack.rb
@@ -2,16 +2,26 @@ require 'formula'
 
 class Lapack < Formula
   homepage 'http://www.netlib.org/lapack/'
-  url 'http://www.netlib.org/lapack/lapack-3.4.0.tgz'
-  md5 '02d5706ec03ba885fc246e5fa10d8c70'
+  url 'http://www.netlib.org/lapack/lapack-3.4.1.tgz'
+  md5 '44c3869c38c8335c2b9c2a8bb276eb55'
+
+  depends_on "homebrew/dupes/openblas" => :optional if ARGV.include? "--openblas"
+  depends_on "cmake" => :build
 
   keg_only :provided_by_osx
 
   def install
     ENV.fortran
 
-    # Copy over make.inc, to load in configuration for this platform
-    mv "INSTALL/make.inc.gfortran", "make.inc"
-    system "make", "PREFIX=#{prefix}", "install"
+    cmake_args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}", "-Wno-dev"]
+
+    if ARGV.include? "--openblas"
+      cmake_args << "-DBLAS_LIBRARIES=#{Formula.factory('openblas').lib}/libopenblas.a"
+    else
+      cmake_args << "-DUSE_OPTIMIZED_BLAS=1"
+    end
+
+    system "cmake", *cmake_args
+    system "make", "install"
   end
 end
